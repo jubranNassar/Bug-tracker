db9d5697fbc1e6370278dcb4205f967e7b610e7a
jubrannassar <jubran.nassar@datadoghq.com>
2022-03-09 20:14:50 -0500

    creates authentication.js, error-handler.js, not-found.js files within the middleware folder for error handling

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 require("dotenv").config()
-const connectDB = require("./db/connect")
 const express = require("express")
 const app = express()
+
+// Routes
 const authRouter = require("./routes/users")
-const port = process.env.PORT || 5000;
+
+// Middleware
+
+// Error Handler
+const notFound = require("./middleware/not-found")
+const errorHandler = require("./middleware/error-handler")
 
 app.get("/", (req, res)=> {
     res.send("Home")
@@ -12,6 +18,14 @@ app.get("/", (req, res)=> {
 app.use(express.json())
 app.use("/api/v1/auth", authRouter)
 
+app.use(notFound)
+app.use(errorHandler)
+
+//MongoDB connect
+const connectDB = require("./db/connect")
+const errorHandlers = require("./middleware/error-handler")
+
+const port = process.env.PORT || 5000;
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
