0923d2ca7c60ee5b19888fee964e92c71f9137ec
mnebot <mnebotjr@gmail.com>
2022-03-10 21:38:35 -0500

    added express-async-errors

--- server.js
+++ server.js
@@ -1,51 +1,47 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+require('dotenv').config();
+require('express-async-errors');
 
+const express = require('express');
+const app = express();
 
 // User authentication
-const authentication = require("./middleware/authentication")
+const authentication = require('./middleware/authentication');
 
 // Routes
-const authRouter = require("./routes/users")
+const authRouter = require('./routes/users');
 
 // Middleware
 
-
 // Error Handler
-const notFound = require("./middleware/not-found")
-const errorHandler = require("./middleware/error-handler")
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.get("/", (req, res)=> {
-    res.send("Home")
-})
+app.get('/', (req, res) => {
+	res.send('Home');
+});
 
-app.use(express.json())
-app.use("/api/v1/auth", authRouter)
+app.use(express.json());
+app.use('/api/v1/auth', authRouter);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 //MongoDB connect
-const connectDB = require("./db/connect")
-const errorHandlers = require("./middleware/error-handler")
+const connectDB = require('./db/connect');
+const errorHandlers = require('./middleware/error-handler');
 
 const port = process.env.PORT || 5000;
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => {
-            console.log(`listening to port ${port}`);
-    });
-    } catch(error) {
-        console.log(error)
-    }
-}
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, () => {
+			console.log(`listening to port ${port}`);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-start()
+start();
 
-
- // TODO: troubleshoot error handling functions by reacreating errors, create user logout function, create a varification function?, start on project model, controller and routes.
-
-
-
+// TODO: troubleshoot error handling functions by reacreating errors, create user logout function, create a varification function?, start on project model, controller and routes.
