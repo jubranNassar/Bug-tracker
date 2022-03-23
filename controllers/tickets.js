728ef6502a4e5f6c040005b2938c2994dc575040
mnebot <mnebotjr@gmail.com>
2022-03-22 21:37:40 -0400

    middleware
    fix auth middleware

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,53 +1,62 @@
-const Ticket = require("../models/ticket")
-const { StatusCodes } = require("http-status-codes")
-const { BadRequestErrors, NotFoundError } = require("../errors")
-
+const Ticket = require('../models/ticket');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestErrors, NotFoundError } = require('../errors');
 
 const getAllTickets = async (req, res) => {
-    const tickets = await Ticket.find({})
-    res.status(StatusCodes.OK).json({ tickets, count: tickets.length })
-}
+	const tickets = await Ticket.find({});
+	res.status(StatusCodes.OK).json({ tickets, count: tickets.length });
+};
 
 const getTicket = async (req, res) => {
-    const { params: { id: ticketId } } = req
-    const ticket = await Ticket.findOne({ _id: ticketId })
-    if (!ticket) {
-        throw new NotFoundError("does not exist")
-    }
-    res.status(StatusCodes.OK).json({ ticket })
-}
+	const {
+		params: { id: ticketId },
+	} = req;
+	const ticket = await Ticket.findOne({ _id: ticketId });
+	if (!ticket) {
+		throw new NotFoundError('does not exist');
+	}
+	res.status(StatusCodes.OK).json({ ticket });
+};
 
 const createTicket = async (req, res) => {
-    const tickets = await Ticket.create(req.body)
-    res.status(StatusCodes.CREATED).json({ tickets })
-}
-
+	const tickets = await Ticket.create(req.body);
+	res.status(StatusCodes.CREATED).json({ tickets });
+};
 
 const editTicket = async (req, res) => {
-    const { params: { id: ticketId, body: title } } = req
-    if (title == "") {
-        throw new BadRequestErrors("Title field cannot be empty")
-    }
-    const ticket = await ticket.findByIdAndUpdate({ _id: ticketId }, req.body, { new: true, runValidators: true })
+	const {
+		params: { id: ticketId, body: title },
+	} = req;
+	if (title == '') {
+		throw new BadRequestErrors('Title field cannot be empty');
+	}
+	const ticket = await ticket.findByIdAndUpdate({ _id: ticketId }, req.body, {
+		new: true,
+		runValidators: true,
+	});
 
-    if (!ticket) {
-        throw new NotFoundError("does not exist")
-    }
+	if (!ticket) {
+		throw new NotFoundError('does not exist');
+	}
 
-    res.status(StatusCodes.OK).json({ ticket })
-}
+	res.status(StatusCodes.OK).json({ ticket });
+};
 
 const deleteTicket = async (req, res) => {
-    const { id: ticketId } = req.params
-    const ticket = await ticket.findOne({ _id: ticketId })
+	const { id: ticketId } = req.params;
+	const ticket = await ticket.findOne({ _id: ticketId });
 
-    if (!ticket) {
-        throw new NotFoundError(`No ticket with ID ${ticketId}`)
+	if (!ticket) {
+		throw new NotFoundError(`No ticket with ID ${ticketId}`);
+	}
+	await ticket.remove();
+	res.status(StatusCodes.OK).json({ msg: 'Deleted successfully' });
+};
 
-    }
-    await ticket.remove()
-    res.status(StatusCodes.OK).json({ msg: "Deleted successfully" })
-
-}
-
-module.exports = { getAllTickets, getTicket, createTicket, editTicket, deleteTicket }
+module.exports = {
+	getAllTickets,
+	getTicket,
+	createTicket,
+	editTicket,
+	deleteTicket,
+};
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,21 +1,23 @@
-const User  = require("../models/auth")
-const jwt = require("jsonwebtoken")
-const { UnauthenticatedError } = require("../errors")
+const User = require('../models/auth');
+const jwt = require('jsonwebtoken');
+const { UnauthenticatedError } = require('../errors');
 
 const auth = async (req, res, next) => {
-    const authHeader = req.headers.authentication
-    if (!authHeader || !authHeader.startsWith("Bearer")) {
-        throw new UnauthenticatedError("Authentication invalid")
-    }
-    const token = authHeader.split(" ")[1]
+	const authHeader = req.headers.authentication;
 
-    try {
-        const payload = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = { userId: payload.userId }
+	if (!authHeader || !authHeader.startsWith('Bearer')) {
+		throw new UnauthenticatedError('Authentication invalid');
+	}
 
-    } catch(error) {
-        throw new UnauthenticatedError("Authentication invalid")
-    }
-}
+	const token = authHeader.split(' ')[1];
 
-module.exports = auth
+	try {
+		const payload = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = { userId: payload.userId };
+		next();
+	} catch (error) {
+		throw new UnauthenticatedError('Authentication invalid');
+	}
+};
+
+module.exports = auth;
